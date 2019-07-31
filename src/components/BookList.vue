32428a385b176f2f90014e5741bb47958cbe0fe9
Ryan Mackenzie <ryan.mackenzie@bekagool.com>
2019-07-31 14:54:45 +0100

    Module 3 - Filtering the Books List

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,8 +1,16 @@
 <template>
   <div>
     <h1>{{title}}</h1>
-    <ul>
+    <ul class="exists">
       <book-item v-for='book in books' :key='book.id' :book='book'></book-item>
+    </ul>
+    <hr>
+    <h2>Filtered Books By Ownership</h2>
+    <select v-model="holding">
+      <option v-for="filter in filters">{{ filter }}</option>
+    </select>
+    <ul>
+      <book-item v-for='book in filteredBooks' :key='book.id' :book='book'></book-item>
     </ul>
     <br><hr>
     <book-form @addBook='appendBook'></book-form>
@@ -10,6 +18,7 @@
 </template>
 
 <script>
+import _ from "lodash";
 import BookItem from "./BookItem";
 import BookForm from "./BookForm";
 
@@ -23,30 +32,42 @@ export default {
         {
           title: "Self-Reliance",
           author: "Ralph Waldo Emerson",
-          finishedReading: true
+          finishedReading: true,
+          ownership: "borrowed"
         },
         {
           title: "American Gods",
           author: "Neil Gaiman",
-          finishedReading: false
+          finishedReading: false,
+          ownership: "bought"
         },
-        { title: "Amusing Ourselves to Death",
+        {
+          title: "Amusing Ourselves to Death",
           author: "Neil Postman",
-          finishedReading: true
+          finishedReading: true,
+          ownership: "borrowed"
         }
-      ]
+      ],
+      filters: ["bought", "borrowed"],
+      holding: "bought"
     };
   },
   components: {
     BookItem,
     BookForm
   },
+  computed: {
+    filteredBooks() {
+        return _.filter(this.books, ["ownership", this.holding]);
+    }
+  },
   methods: {
     appendBook(bookData) {
       this.books.push({
         title: bookData.bookTitle,
         author: bookData.bookAuthor,
-        finishedReading: bookData.finishedReading
+        finishedReading: bookData.finishedReading,
+        ownership: bookData.ownership
       });
     }
   }
